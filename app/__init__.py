a76cf8891ffa022bf2600bc72c79f6c0a9b544e2
Idris Abdurrahman <abdurrahmaneedrees@gmail.com>
2025-02-03 14:58:24 +0100

    Add module docstring and refactor import in

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
+"""
+This module initializes the Flask application, applies CORS settings,
+and registers the main blueprint for the application routes.
+"""
+
 from flask import Flask
 from flask_cors import CORS
+from app.routes import main_bp
 
 
 def create_app():
+    """
+      Create and configure the Flask application.
+
+      This function initializes the Flask application, applies CORS settings,
+      and registers the main blueprint for the application routes.
+
+      Returns:
+          Flask: The configured Flask application instance.
+      """
     app = Flask(__name__)
     CORS(app)
 
-    from app.routes import main_bp
+
     app.register_blueprint(main_bp)
-    
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, request
+"""
+This module contains the route definitions for the Flask application.
+"""
 
+from flask import Blueprint, request
 from app.controllers import classify
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/api/classify-number', methods=['GET'])
 def classify_number():
-    number = request.args.get('number')
+    """
+    Endpoint to classify a number.
 
+    This route handles GET requests to the '/api/classify-number' URL.
+    It expects a query parameter 'number' and uses the classify function
+    from the app.controllers module to classify the number.
+
+    Returns:
+        Response: The result of the classify function.
+    """
+    number = request.args.get('number')
     return classify(number)
